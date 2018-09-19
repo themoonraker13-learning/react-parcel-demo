3d560af52b1ce031f23fb6371662df62d7ed2e89
Parth Kachchhi <themoonraker13@gmail.com>
2018-09-19 12:24:23 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('Hello Parcel Project');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello Parcel React Project</h1>,
+  document.getElementById('root')
+);
